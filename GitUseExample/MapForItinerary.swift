a4d2aa07fccc054b35634698f6dfd18a5b9fe627
chipcastille <chipcastille@yahoo.com>
2018-09-19 13:43:40 -0400

    Add map class

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Chip Castille on 9/19/18.
+//  Copyright © 2018 Chip Castille. All rights reserved.
+//
+
+import Foundation
+
+class MapForItinerary: NSObject {
+    
+}
